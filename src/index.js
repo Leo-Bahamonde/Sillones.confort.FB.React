eb15ee5e1cbf4cc342ee066ce3f3d9a91f589a6a
Leonel Bahamonde <leoebahamonde@gmail.com>
2021-06-12 19:12:35 -0300

    make it better2

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ import Products from './components/Navbars/Products'
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      <Route path="/sillones-confort-fb.git" exact>
+      <Route path="/" exact>
         <App/> 
       </Route>
       <Route path="/productos">
